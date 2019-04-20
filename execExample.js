da2e7a50e23dfda30c4614fc611a1e1760b9e13d
Parveen Kumar <parveen.kumar@cloudwords.com>
2019-04-20 15:39:20 +0530

    Child process examples

---
+++ execExample.js
@@ -0,0 +1,10 @@
+// spawn a shell by default
+const { exec } = require('child_process');
+exec('ls | wc -l', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.log(`stderr: ${stderr}`);
+});
---
+++ execFile.js
@@ -0,0 +1,16 @@
+// const { execFile } = require('child_process');
+// const child = execFile('node', ['--version'], (error, stdout, stderr) => {
+//   if (error) {
+//     throw error;
+//   }
+//   console.log(stdout);
+// });
+
+// promisify
+const util = require('util');
+const execFile = util.promisify(require('child_process').execFile);
+async function getVersion() {
+  const { stdout } = await execFile('node', ['--version']);
+  console.log(stdout);
+}
+getVersion();
